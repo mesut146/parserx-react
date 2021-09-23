f4dc71ac43309d891c5fbcdf99d319eb11156820
Mesut <mesutdogansoy@gmail.com>
2021-09-23 23:21:26 +0300

    design

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-alert("js");
+var url = "https://parserx-rest.herokuapp.com/";
+
+function prepare(input) {
+  var out = "";
+  var cur = "";
+  for (let ch in input) {
+    if (ch == " ") {
+      if (cur.length > 0) {
+        out += ' "' + cur + '"';
+        cur = "";
+      }
+    } else {
+      cur += ch;
+    }
+  }
+  if (cur.length > 0) {
+    out += ' "' + cur + '"';
+  }
+  return out;
+}
+
+function left() {
+  let input = window.document.getElementById("input");
+  let output = window.document.getElementById("output");
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", url);
+  xmlHttp.send(null);
+  let out = xmlHttp.responseText;
+  output.innerHTML = out;
+}
